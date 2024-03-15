3284c531b10c472b12b1591f3a40ae0360b08e58
gvheisler <gvheisler@inf.ufsm.br>
2024-03-15 12:26:05 -0300

    novo compilador de relatórios funcionando. Falta criar a função de semestre

---
+++ R/main_script.R
@@ -0,0 +1,25 @@
+library(readxl)
+library(dplyr)
+
+arquivos <- list.files(path = "relatorios", pattern = "\\.xlsx$", full.names = TRUE)
+
+ler_e_remover <- function(arquivo) {
+  dados <- read_excel(arquivo)
+  if ("COD_CURSO" %in% colnames(dados) && "NOME_CURSO" %in% colnames(dados)) {
+    dados <- select(dados, -c(COD_CURSO, NOME_CURSO))
+  }
+  return(dados)
+}
+
+dados_totais <- lapply(arquivos, ler_e_remover)
+dados_totais <- bind_rows(dados_totais)
+
+disciplinas <- read_excel("Disciplinas_curriculo2024.xlsx")
+
+names(disciplinas)[names(disciplinas) == "COD"] <- "Cod"
+
+merged_data <- merge(ds2, disciplinas[, c("Cod", "Sem")], by.x = "COD_DISCIPLINA", by.y = "Cod", all.x = TRUE)
+
+merged_data$Sem[is.na(merged_data$Sem)] <- "Inativo"
+
+write.csv(merged_data, "resultado_final.csv")
--- R/merge_disciplinas.R
+++
@@ -1,5 +0,0 @@
-library(readxl)
-library(openxlsx)
-
-ds1 <- read_excel("..\\data\\Dados.xlsx")
-ds1 <- read_excel("..\\data\\2024.xls")
