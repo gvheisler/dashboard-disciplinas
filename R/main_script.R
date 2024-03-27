d245af59d9a9574e25695a4072c0173ccfa4a6c1
gvheisler <gvheisler@inf.ufsm.br>
2024-03-27 12:26:22 -0300

    codigo alterado para manter o nome do curso

--- R/main_script.R
+++ R/main_script.R
@@ -6,7 +6,7 @@ arquivos <- list.files(path = "relatorios", pattern = "\\.xlsx$", full.names = T
 ler_e_remover <- function(arquivo) {
   dados <- read_excel(arquivo)
   if ("COD_CURSO" %in% colnames(dados) && "NOME_CURSO" %in% colnames(dados)) {
-    dados <- select(dados, -c(COD_CURSO, NOME_CURSO))
+    dados <- select(dados, -c(COD_CURSO))
   }
   return(dados)
 }
@@ -21,5 +21,8 @@ names(disciplinas)[names(disciplinas) == "COD"] <- "Cod"
 merged_data <- merge(dados_totais, disciplinas[, c("Cod", "Sem")], by.x = "COD_DISCIPLINA", by.y = "Cod", all.x = TRUE)
 
 merged_data$Sem[is.na(merged_data$Sem)] <- "Inativo"
+
+merged_data$NOME_CURSO <- ifelse(is.na(merged_data$NOME_CURSO), sample(na.omit(merged_data$NOME_CURSO), size = sum(is.na(merged_data$NOME_CURSO)), replace = TRUE), merged_data$NOME_CURSO)
+
 
 write.csv(merged_data, "disciplinas_completo.csv")
